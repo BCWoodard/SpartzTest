5573ae38ab30ddb252daf35e5eb1045130c8fb4b
Brad Woodard <bcwoodard@gmail.com>
2013-10-18 10:54:39 -0500

    Corrected spelling

--- SpartzInc/SPZConstants.h
+++ SpartzInc/SPZConstants.h
@@ -17,7 +17,7 @@ extern NSString *const NUMBER_OF_TWEETS;
 extern NSString *const TIMELINE_TO_RETRIEVE;
 extern NSString *const RETRIEVE_ENTITIES;
 
-// bit AND / arithmatic shift for setting the color an item
+// bit AND / arithmetic shift for setting the color an item
 #define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 
 @end
