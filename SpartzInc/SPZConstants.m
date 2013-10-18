610ebbead9ae9f3378b9b53ee06ce9cbcff8eca8
Brad Woodard <bcwoodard@gmail.com>
2013-10-18 01:47:22 -0500

    Changed #tweets to 20

--- SpartzInc/SPZConstants.m
+++ SpartzInc/SPZConstants.m
@@ -12,7 +12,7 @@
 
 NSString *const PROFILE_IMAGE_NOTIFICATION = @"Profile Image Found";
 NSString *const TWITTER_FEED_URL = @"https://api.twitter.com/1.1/statuses/user_timeline.json";
-NSString *const NUMBER_OF_TWEETS = @"30";
+NSString *const NUMBER_OF_TWEETS = @"20";
 NSString *const TIMELINE_TO_RETRIEVE = @"SpartzInc";
 NSString *const RETRIEVE_ENTITIES = @"1";
 
